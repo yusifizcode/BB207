5565054bb9e4f1ab87d2de1d6b43498031af5065
yusifizcode <5ba0gh2@code.edu.az>
2024-03-08 16:04:09 +0400

    Conditions, Loops

---
+++ LoopsAndConditions/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,86 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Task 1
+            //int sum = 0;
+            //for (int i = 2; i < 100; i += 2)
+            //{
+            //    sum += i;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region Task 2
+            //int product = 1;
+            //int a = 1;
+            //int b = 10;
+            //for (int i = a; i <= b; i++)
+            //{
+            //    product *= i;
+            //}
+            //Console.WriteLine(product);
+            #endregion
+
+            #region Task 3
+            //int m = 100;
+            //int sum = 0;
+            //int count = 0;
+            //int avg = 0;
+            //for (int i = 1; i < m; i++)
+            //{
+            //    if (i % 15 == 0)
+            //    {
+            //        count++;
+            //        sum += i;
+            //    }
+            //}
+
+            //avg = sum / count;
+            //Console.WriteLine(avg);
+            #endregion
+
+            #region Task 4
+            //int n = 40;
+            ////bool check = false;
+            //int count = 0;
+            //for (int i = n; i < 100; i++)
+            //{
+            //    if (i % 21 == 0)
+            //    {
+            //        count++;
+            //        break;
+            //    }
+            //}
+
+            //if (count > 0)
+            //{
+            //    Console.WriteLine("Var");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Yoxdur");
+            //}
+            #endregion
+
+            #region Task 5
+            //int n = 12435;
+
+            //int qaliq = 0;
+            //int cem = 0;
+
+            //while (n != 0)
+            //{
+            //    qaliq = n % 10;
+            //    cem += qaliq;
+            //    n = n - qaliq;
+            //    n = n / 10;
+            //}
+
+            //Console.WriteLine(cem);
+            #endregion
+        }
+    }
+}
